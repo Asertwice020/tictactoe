d380a51f24a265806f3e762af3ad21d0e878736b
Asertwice020 <sumitsaraswat>
2023-12-07 01:27:38 +0530

    add: asset folder which contains, tap & restart sounds. and these sounds are play when user tap and restart.

--- ✖️🔴GAME/logic.js
+++ ✖️🔴GAME/logic.js
@@ -1,13 +1,47 @@
 'use strict';
 function initializeGame() {
-  const gameContainer = document.querySelector('.game_container');
+const gameContainer = document.querySelector('.game_container');
 const boxes = [...gameContainer.children];
+
+// AUDIO VARIABLES
+const tapAudio = new Audio('Assets/tapAudio.mp3');
+tapAudio.preload = 'auto';
+const restartAudio = new Audio('Assets/restartAudio.mp3');
+restartAudio.preload = 'auto'
+  // FLAG TO CHECK IF THE SOUND IS ALREADY PLAYING
+let isTapAudioPlaying = true;
+let isRestartAudioPlaying = true;
 
 let winCard;
 let restartBtn;
 let gameOverWindow;
 let currentPlayer = 'X';
 let gameOver = false;
+
+function playTapAudio() {
+  if(!isTapAudioPlaying) {
+    // If the sound is not currently playing
+    // Set the flag to indicate that the sound is now playing
+    isTapAudioPlaying = true;
+  }
+  else {
+    // If the sound is already playing
+    // Rewind the audio to the beginning
+    tapAudio.currentTime = 0;
+  }
+  // AFTER CONDITIONS CHECKED, play the sound!
+  tapAudio.play();
+}
+
+function playRestartAudio() {
+  if(!isRestartAudioPlaying) {
+    isRestartAudioPlaying = true;
+  }
+  else {
+    restartAudio.currentTime = 0;
+  }
+  restartAudio.play();
+}
 
 function restartGame() {
   restartBtn.addEventListener('click', instructionsToRestart)
@@ -19,6 +53,7 @@ function instructionsToRestart() {
     for (const box of boxes) box.innerText = '';
     currentPlayer = 'X';
     gameOverWindow.remove();
+    playRestartAudio();
   }
 }
 
@@ -62,6 +97,7 @@ function playMove(event) {
   if (!gameOver && event.target.innerText === '') {
     event.target.innerText = currentPlayer;
     currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+    playTapAudio();
     checkWinner();
   }
 }
